58f1d8298284c48c139487227f646657dec9383c
Pablo M. Ocampos <pablo.mk.ocampos@gmail.com>
2025-12-23 08:40:14 -0300

    prueba produccion 5 imagenes

--- AppFront/src/utils/HostLocation.js
+++ AppFront/src/utils/HostLocation.js
@@ -3,7 +3,7 @@ export const HostLocation = (op = 0) => {
 
     let protocol = window.location.protocol;
     let hostname = window.location.hostname;
-    let port = hostname === 'localhost' ? ':8082' : '/biotech';
+    let port = hostname === 'localhost' ? ':8082' : '';
 
     if (op === 1) return `${protocol}//${hostname}${port}`;
     else return `${protocol}//${hostname}${port}/api`;
